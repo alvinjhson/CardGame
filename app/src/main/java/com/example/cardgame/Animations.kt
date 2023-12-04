c8faf4556744695fa51bf26bd3824968a0ed1751
alvinjhson <alvinbest26@gmail.com>
2023-12-04 12:22:41 +0100

    fixed animation class

---
+++ app/src/main/java/com/example/cardgame/Animations.kt
@@ -0,0 +1,39 @@
+package com.example.cardgame
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.widget.ImageView
+
+class Animations {
+
+
+    fun dealerCardAnimation(view: ImageView) {
+        val leftAnimation = ObjectAnimator.ofFloat(view, "translationX", -500f).apply {
+            duration = 250
+        }
+        val rightAnimation = ObjectAnimator.ofFloat(view, "translationX", 50f).apply{
+            duration = 250
+        }
+        AnimatorSet().apply {
+            play(leftAnimation).with(rightAnimation)
+            play(rightAnimation).with(leftAnimation)
+            start()
+        }
+    }
+
+    fun playerCardAnimation(view: ImageView) {
+        val leftAnimation = ObjectAnimator.ofFloat(view, "translationX", -500f).apply {
+            duration = 250
+        }
+        val rightAnimation = ObjectAnimator.ofFloat(view, "translationX", 50f).apply{
+            duration = 250
+        }
+        AnimatorSet().apply {
+            play(leftAnimation).with(rightAnimation)
+            play(rightAnimation).with(leftAnimation)
+            start()
+        }
+    }
+
+
+}
