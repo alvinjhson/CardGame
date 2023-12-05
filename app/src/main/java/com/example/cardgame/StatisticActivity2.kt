6ab723c568dd859f89c08984eeaf4431d3855acd
alvinjhson <alvinbest26@gmail.com>
2023-12-05 16:05:52 +0100

    Made new design for statistic

--- app/src/main/java/com/example/cardgame/StatisticActivity2.kt
+++ app/src/main/java/com/example/cardgame/StatisticActivity2.kt
@@ -4,16 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import org.w3c.dom.Text
 
+
 class StatisticActivity2 : AppCompatActivity() {
+
+    var play = PlayActivity()
+
     lateinit var playerWinsView : TextView
     lateinit var dealerWinsView : TextView
     lateinit var roundsPlayedWins : TextView
     lateinit var drawsView : TextView
     lateinit var playerBalanceView : TextView
     lateinit var totalChipsWonView: TextView
+    lateinit var playerNameView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +30,9 @@ class StatisticActivity2 : AppCompatActivity() {
         drawsView = findViewById(R.id.drawView)
         playerBalanceView = findViewById(R.id.playerBalanceView)
         totalChipsWonView = findViewById(R.id.totalChipsWonView)
+        playerNameView = findViewById(R.id.playerTextView)
 
-        val homeButton = findViewById<Button>(R.id.homeButton)
+        val homeButton = findViewById<ImageButton>(R.id.homeButton)
         val playerWins = intent.getIntExtra("playerWins", 0)
         val dealerWins = intent.getIntExtra("dealerWins", 0)
         val draws = intent.getIntExtra("draws", 0)
@@ -39,6 +46,7 @@ class StatisticActivity2 : AppCompatActivity() {
             showRoundsPlayed(roundsPlayed)
             showPlayerBalance(playerBalance)
             showTotalChipsWon(totalChipsWon)
+            showPlayerName()
 
             homeButton.setOnClickListener {
                 val intent = Intent(this, MainActivity::class.java)
@@ -63,10 +71,15 @@ class StatisticActivity2 : AppCompatActivity() {
     }
     fun showPlayerBalance(playerBalance: Int) {
         val playerBalance = playerBalance.toString()
-        playerBalanceView.text = "Balance $playerBalance"
+        playerBalanceView.text = " $playerBalance $"
     }
     fun showTotalChipsWon(totalChipsWon: Int) {
         val totalChipWon = totalChipsWon.toString()
         totalChipsWonView.text = "Total Chips Won $totalChipWon"
+    }
+    fun showPlayerName() {
+       val name = play.chips[0].name
+        playerNameView.text = "$name"
+
     }
 }
